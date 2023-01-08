4bb37ad8fe2ce565b40d34ccd75335cfa005049d
whes1015 <whes1015@gmail.com>
2023-01-08 13:08:45 +0800

    refactor: report

--- core/api.js
+++ core/api.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
-const { shell } = require("@electron/remote");
 const fs = require("fs");
+const { shell } = require("@electron/remote");
 const region = JSON.parse(fs.readFileSync("./resource/data/region.json").toString());
 
 function Now() {
@@ -19,6 +19,7 @@ function refresh_report_list() {
 			const report_list = document.getElementById("report_list");
 			report_list.innerHTML = "";
 			for (let i = 0; i < ans.length; i++) {
+				const originTime = new Date((new Date(`${ans[i].originTime} GMT+08:00`)).toLocaleString("en-US", { timeZone: "Asia/Taipei" }));
 				const intensity = ans[i].data[0]?.areaIntensity ?? 0;
 				const time = ans[i].originTime.substring(0, 16);
 				let loc = ans[i].location;
@@ -27,12 +28,18 @@ function refresh_report_list() {
 				const resize = (intensity > 4 && intensity != 7) ? true : false;
 				const intensity_level = (intensity == 0) ? "--" : int_to_intensity(intensity);
 				if (i == 0)
-					report.innerHTML = `<div class="report"><div class="report_text report_intensity intensity_${intensity}"style="font-size: ${(resize) ? "50" : "60"}px;">${intensity_level}</div><div class="report_text_box"><div class="report_text" style="font-size: 22px;"><b>${loc}</b></div><div class="report_text" style="font-size: 15px;">${time}</div><div style="display: flex;"><div class="report_text"><b>M&nbsp;${ans[i].magnitudeValue.toFixed(1)}</b></div><div class="report_text report_scale" style="width: 100%;text-align: right;">深度:&nbsp;<b>${ans[i].depth}</b>&nbsp;km</div></div></div></div>`;
+					report.innerHTML = `<div class="report"><div id="${originTime}_info" class="report_item"><div class="report_text report_intensity intensity_${intensity}"style="font-size: ${(resize) ? "50" : "60"}px;">${intensity_level}</div><div class="report_text_box"><div class="report_text" style="font-size: 22px;"><b>${loc}</b></div><div class="report_text" style="font-size: 15px;">${time}</div><div style="display: flex;"><div class="report_text"><b>M&nbsp;${ans[i].magnitudeValue.toFixed(1)}</b></div><div class="report_text report_scale" style="width: 100%;text-align: right;">深度:&nbsp;<b>${ans[i].depth}</b>&nbsp;km</div></div></div></div><div id="${originTime}_click_box" class="report_click hide"><i id="${originTime}_click_replay" style="color: red;" class="report_click_text fa-regular fa-circle-play fa-2x" style="color: white;"></i><i id="${originTime}_click_web" style="${ans[i].location.startsWith("TREM 人工定位") ? "color: red;" : ""}" class="report_click_text fa fa-globe fa-2x"></i></div></div>`;
 				else
-					report.innerHTML = `<div class="report"><div class="report_text report_intensity intensity_${intensity}"style="font-size: ${(resize) ? "35" : "40"}px;max-width: 55px;">${intensity_level}</div><div class="report_text_box"><div class="report_text"><b>${loc}</b></div><div class="report_text" style="font-size: 15px;">${time}</div></div><div class="report_text report_scale"><b>M&nbsp;${ans[i].magnitudeValue.toFixed(1)}</b></div></div>`;
-				report.addEventListener("click", () => {
-					if (ans[i].location.startsWith("TREM 人工定位")) return;
-					const originTime = new Date((new Date(`${ans[i].originTime} GMT+08:00`)).toLocaleString("en-US", { timeZone: "Asia/Taipei" }));
+					report.innerHTML = `<div class="report"><div id="${originTime}_info" class="report_item"><div class="report_text report_intensity intensity_${intensity}" style="font-size: ${(resize) ? "35" : "40"}px;max-width: 55px;">${intensity_level}</div><div class="report_text_box"><div class="report_text"><b>${loc}</b></div><div class="report_text" style="font-size: 15px;">${time}</div></div><div class="report_text report_scale"><b>M&nbsp;${ans[i].magnitudeValue.toFixed(1)}</b></div></div><div id="${originTime}_click_box" class="report_click hide"><i id="${originTime}_click_replay" style="color: red;" class="report_click_text fa-regular fa-circle-play fa-2x" style="color: white;"></i><i id="${originTime}_click_web" style="${ans[i].location.startsWith("TREM 人工定位") ? "color: red;" : ""}" class="report_click_text fa fa-globe fa-2x"></i></div></div>`;
+				report.addEventListener("mouseenter", () => {
+					document.getElementById(`${originTime}_info`).style.visibility = "hidden";
+					document.getElementById(`${originTime}_click_box`).className = "report_click";
+				});
+				report.addEventListener("mouseleave", () => {
+					document.getElementById(`${originTime}_info`).style.visibility = "visible";
+					document.getElementById(`${originTime}_click_box`).className = "report_click hide";
+				});
+				if (!ans[i].location.startsWith("TREM 人工定位")) {
 					const cwb_code = "EQ"
 						+ ans[i].earthquakeNo
 						+ "-"
@@ -42,12 +49,15 @@ function refresh_report_list() {
 						+ (originTime.getHours() < 10 ? "0" : "") + originTime.getHours()
 						+ (originTime.getMinutes() < 10 ? "0" : "") + originTime.getMinutes()
 						+ (originTime.getSeconds() < 10 ? "0" : "") + originTime.getSeconds();
-					shell.openExternal(`https://www.cwb.gov.tw/V8/C/E/EQ/${cwb_code}.html`);
-				});
+					report.addEventListener("click", () => {
+						shell.openExternal(`https://www.cwb.gov.tw/V8/C/E/EQ/${cwb_code}.html`);
+					});
+				}
 				report_list.appendChild(report);
 			}
 		})
 		.catch((err) => {
+			console.log(err);
 			setTimeout(() => refresh_report_list(), 5000);
 		});
 }
