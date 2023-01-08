d4a0e9ea34823f0c052e1e80761e41b4ec75961e
夜夜 <vcde0000@gmail.com>
2023-01-09 05:18:36 +0800

    chore: focus-visible none

--- core/event.js
+++ core/event.js
@@ -8,6 +8,7 @@ document.addEventListener("keydown", (event) => {
 
 	if (event.ctrlKey && event.shiftKey && event.key.toLocaleLowerCase() == "i")
 		ipcRenderer.send("openDevtool");
+
 	if (event.ctrlKey && event.key.toLocaleLowerCase() == "r")
 		ipcRenderer.send("reloadpage");
 });
