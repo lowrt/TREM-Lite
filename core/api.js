24f5b72aee543277a4a33428f0fdb306556fcb78
夜夜 <vcde0000@gmail.com>
2023-01-10 11:07:26 +0800

    fix: setting language module

--- core/api.js
+++ core/api.js
@@ -1,23 +1,8 @@
 /* eslint-disable no-undef */
 const { BrowserWindow, shell } = require("@electron/remote");
-const fs = require("fs");
-const path = require("path");
-const region = JSON.parse(fs.readFileSync(path.resolve(app.getAppPath(), "./resource/data/region.json")).toString());
 const win = BrowserWindow.fromId(process.env.window * 1);
 
 let report_data = {};
-
-const tw_lang_data = JSON.parse(fs.readFileSync(path.resolve(app.getAppPath(), `./resource/lang/${localStorage.lang ?? "zh-Hant"}.json`)).toString());
-let lang_data = {};
-try {
-	lang_data = JSON.parse(fs.readFileSync(path.resolve(app.getAppPath(), `./resource/lang/${localStorage.lang ?? "zh-Hant"}.json`)).toString());
-} catch (err) {
-	console.log(err);
-}
-
-function get_lang_string(id) {
-	return lang_data[id] ?? tw_lang_data[id] ?? "";
-}
 
 function Now() {
 	return new Date(ServerTime + (Date.now() - ServerT));
--- core/globals.js
+++ core/globals.js
@@ -1,2 +1,17 @@
 const { app } = require("@electron/remote");
 const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
+const region = JSON.parse(fs.readFileSync(path.resolve(app.getAppPath(), "./resource/data/region.json")).toString());
+
+const tw_lang_data = JSON.parse(fs.readFileSync(path.resolve(app.getAppPath(), `./resource/lang/${localStorage.lang ?? "zh-Hant"}.json`)).toString());
+let lang_data = {};
+try {
+	lang_data = JSON.parse(fs.readFileSync(path.resolve(app.getAppPath(), `./resource/lang/${localStorage.lang ?? "zh-Hant"}.json`)).toString());
+} catch (err) {
+	console.log(err);
+}
+
+function get_lang_string(id) {
+	return lang_data[id] ?? tw_lang_data[id] ?? "";
+}
