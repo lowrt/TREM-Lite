3c7aa36d17e22d583ccb212bb3ab6fc2d4626a68
whes1015 <whes1015@gmail.com>
2023-01-10 12:16:46 +0800

    fix: lang

--- core/setting.js
+++ core/setting.js
@@ -1,6 +1,7 @@
-document.getElementById("setting_message").innerHTML = `${get_lang_string("setting.message")}`;
-document.getElementById("setting_graphics").innerHTML = `${get_lang_string("setting.graphics")}`;
-document.getElementById("setting_sound_effects").innerHTML = `${get_lang_string("setting.sound-effects")}`;
-document.getElementById("setting_language").innerHTML = `${get_lang_string("setting.language")}`;
-document.getElementById("setting_plug_in").innerHTML = `${get_lang_string("setting.plug-in")}`;
-document.getElementById("setting_about").innerHTML = `${get_lang_string("setting.about")}`;
+/* eslint-disable no-undef */
+document.getElementById("setting_message").innerHTML = get_lang_string("setting.message");
+document.getElementById("setting_graphics").innerHTML = get_lang_string("setting.graphics");
+document.getElementById("setting_sound_effects").innerHTML = get_lang_string("setting.sound-effects");
+document.getElementById("setting_language").innerHTML = get_lang_string("setting.language");
+document.getElementById("setting_plug_in").innerHTML = get_lang_string("setting.plug-in");
+document.getElementById("setting_about").innerHTML = get_lang_string("setting.about");
