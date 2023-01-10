74cbae5b30e3fbe4a151ab6312f7a114e1d8441f
whes1015 <whes1015@gmail.com>
2023-01-10 23:54:27 +0800

    feat: setting

--- core/globals.js
+++ core/globals.js
@@ -1,10 +1,11 @@
-const { ipcRenderer } = require("electron");
-const { BrowserWindow, shell, app } = require("@electron/remote");
+const { BrowserWindow, app, shell } = require("@electron/remote");
 const fetch = require("node-fetch");
 const fs = require("fs");
+const { ipcRenderer } = require("electron");
 const path = require("path");
 const region = JSON.parse(fs.readFileSync(path.resolve(app.getAppPath(), "./resource/data/region.json")).toString());
 const lang = {};
+
 let tw_lang_data = {};
 let lang_data = {};
 
@@ -21,20 +22,20 @@ fs.readdirSync(path.join(app.getAppPath(), "./resource/lang/")).forEach((file, i
 			} catch (err) {
 				console.log(err);
 			}
-            const currentWindow = BrowserWindow.getFocusedWindow();
-            console.log(currentWindow.title);
-            const head = document.getElementsByTagName('head')[0];
-            var link = document.createElement('link');
-            link.type='text/css';
-            link.rel = 'stylesheet';
-            if (currentWindow.title == "TREM-Lite")
-                link.href = `../resource/lang/${path.parse(file).name}/css/main.css`;
-            else if (currentWindow.title == "TREM-Lite Setting")
-                link.href = `../resource/lang/${path.parse(file).name}/css/setting.css`;
-            head.appendChild(link);
+			const currentWindow = BrowserWindow.getFocusedWindow();
+			console.log(currentWindow.title);
+			const head = document.getElementsByTagName("head")[0];
+			const link = document.createElement("link");
+			link.type = "text/css";
+			link.rel = "stylesheet";
+			if (currentWindow.title == "TREM-Lite")
+				link.href = `../resource/lang/${path.parse(file).name}/css/main.css`;
+			else if (currentWindow.title == "TREM-Lite Setting")
+				link.href = `../resource/lang/${path.parse(file).name}/css/setting.css`;
+			head.appendChild(link);
 		}
-	} catch (error) {
-		console.error(error);
+	} catch (err) {
+		console.error(err);
 	}
 });
 
