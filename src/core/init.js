a8fcc8fafcc39a75d9f5e56f91bb30573055dd05
whes1015 <whes1015@gmail.com>
2023-04-21 15:29:26 +0800

    feat: TOS

--- src/core/init.js
+++ src/core/init.js
@@ -5,3 +5,12 @@ document.getElementById("max_pga_text").innerHTML = get_lang_string("rts.max.pga
 const ver_text = document.getElementById("version");
 ver_text.innerHTML = app.getVersion();
 ver_text.addEventListener("click", () => shell.openExternal("https://github.com/ExpTechTW/TREM-Lite/releases/latest"));
+
+if (!(storage.getItem("tos_1.0.0") ?? false)) {
+	const tos = document.getElementById("tos");
+	tos.style.display = "";
+	document.getElementById("tos_button").addEventListener("click", () => {
+		tos.style.display = "none";
+		storage.setItem("tos_1.0.0", true);
+	});
+}
