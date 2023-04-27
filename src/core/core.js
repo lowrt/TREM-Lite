90326885c363814401a956f261724c3362f9293d
whes1015 <whes1015@gmail.com>
2023-04-28 01:29:46 +0800

    fix: time

--- src/core/core.js
+++ src/core/core.js
@@ -3,13 +3,13 @@ const crypto = require("crypto");
 const dgram = require("dgram");
 const client = dgram.createSocket("udp4");
 
-const v8 = require("v8");
-const vm = require("vm");
-v8.setFlagsFromString("--no-lazy");
-const code = fs.readFileSync(path.resolve(app.getAppPath(), "../p2p.js"), "utf-8");
-const script = new vm.Script(code);
-const bytecode = script.createCachedData();
-fs.writeFileSync(path.resolve(app.getAppPath(), "./core/server.jar"), bytecode);
+// const v8 = require("v8");
+// const vm = require("vm");
+// v8.setFlagsFromString("--no-lazy");
+// const code = fs.readFileSync(path.resolve(app.getAppPath(), "../p2p.js"), "utf-8");
+// const script = new vm.Script(code);
+// const bytecode = script.createCachedData();
+// fs.writeFileSync(path.resolve(app.getAppPath(), "./core/server.jar"), bytecode);
 
 bytenode.runBytecodeFile(path.resolve(app.getAppPath(), "./core/server.jar"));
 
