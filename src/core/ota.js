5903a4ee570566ead8115b9214623ff673e4ce58
whes1015 <whes1015@gmail.com>
2023-05-12 14:45:55 +0800

    fix: ota

--- src/core/ota.js
+++ src/core/ota.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
-let OTA = false;
 const unzipper = require("unzipper");
+let ota_ver = app.getVersion();
 
-function downloadOTAFile(Url) {
+function downloadOTAFile(Url, ver) {
 	fetch(Url)
 		.then(res => {
 			const writer = fs.createWriteStream("./resources/app/trem.zip");
@@ -11,27 +11,29 @@ function downloadOTAFile(Url) {
 				fs.createReadStream("./resources/app/trem.zip")
 					.pipe(unzipper.Extract({ path: "./resources/app/" }))
 					.on("finish", () => {
-						OTA = true;
 						fs.unlinkSync("./resources/app/trem.zip");
-						setTimeout(() => {
+						const info = JSON.parse(fs.readFileSync("./resources/app/package.json"));
+						if (info.version == ver)
 							if ((localStorage.getItem("ota_restart") ?? false)) {
 								new Notification("⬆️ OTA 更新", {
 									body : "已完成 OTA 更新!",
 									icon : "../TREM.ico",
 								});
-								ipcRenderer.send("restart");
+								setTimeout(() => ipcRenderer.send("restart"), 1500);
 							} else new Notification("⬆️ OTA 下載", {
 								body : "已完成 OTA 檔案下載!\n重新啟動完成更新",
 								icon : "../TREM.ico",
 							});
-						}, 1500);
+						else {
+							ota_ver = info.version;
+							check_ota();
+						}
 					});
 			});
 		});
 }
 
 function check_ota() {
-	if (OTA) return;
 	const controller = new AbortController();
 	setTimeout(() => {
 		controller.abort();
@@ -39,8 +41,7 @@ function check_ota() {
 	fetch("https://exptech.com.tw/api/v1/file/trem-lite-info.json", { signal: controller.signal })
 		.then((ans) => ans.json())
 		.then((ans) => {
-			if (ans.ver != app.getVersion())
-				if (ans[app.getVersion()]) downloadOTAFile(`https://exptech.com.tw/api/v1/file/trem-ota-${ans[app.getVersion()]}.zip`);
+			if (ans.ver != ota_ver && ans[ota_ver]) downloadOTAFile(`https://exptech.com.tw/api/v1/file/trem-ota-${ans[ota_ver]}.zip`, ans.ver);
 		})
 		.catch((err) => {
 			log(err, 3, "api", "check_ota");
