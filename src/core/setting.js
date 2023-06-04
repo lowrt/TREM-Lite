4bca4c59f71f040e7298192c305cc3c603457945
Kamiya <58339640+kamiya10@users.noreply.github.com>
2023-06-04 09:32:52 +0800

    chore: style tweaks

--- src/core/setting.js
+++ src/core/setting.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 document.getElementById("setting_message").innerHTML = get_lang_string("setting.general");
 document.getElementById("client-version").innerHTML = app.getVersion();
-document.getElementById("client-uuid").title = `${localStorage.UUID}`;
+document.getElementById("client-uuid").title = `點擊複製 UUID\n${localStorage.UUID}`;
 document.getElementById("client-uuid").addEventListener("click", () => {
 	navigator.clipboard.writeText(localStorage.UUID).then(() => {
 		console.log(localStorage.UUID);
