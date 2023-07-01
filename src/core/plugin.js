e54c814f818ef37ec117ab72cf2c33f3c859cbd4
whes1015 <whes1015@gmail.com>
2023-07-02 01:08:53 +0800

    fix: plugin

--- src/core/plugin.js
+++ src/core/plugin.js
@@ -10,22 +10,24 @@ function load_plugin() {
 	};
 	for (const i of plugin_list)
 		try {
-			const f = reload(Path + i + "/index.js");
-			const info = JSON.parse(fs.readFileSync(Path + i + "/trem.json").toString());
-			const config = JSON.parse(fs.readFileSync(Path + i + "/config.json").toString());
-			if (ver_string_to_int(app.getVersion()) < ver_string_to_int(info.dependencies?.trem ?? "0.0.0")) {log(`Plugin failed to load (${i})`, 2, "plugin", "load_plugin");} else {
-				log(`Plugin loaded successfully (${i})`, 1, "plugin", "load_plugin");
-				if (f.start) f.start();
-			}
-			plugin_info[i] = {
-				f,
-				version      : info.version ?? "0.0.0",
-				description  : info.description ?? "作者未添加說明",
-				author       : info.author ?? ["TREM"],
-				dependencies : info.dependencies ?? {},
-				link         : info.link ?? "https://github.com/ExpTechTW/TREM-Lite",
-				config,
-			};
+			if (fs.existsSync(Path + i + "/index.js")) {
+				const f = reload(Path + i + "/index.js");
+				const info = JSON.parse(fs.readFileSync(Path + i + "/trem.json").toString());
+				const config = JSON.parse(fs.readFileSync(Path + i + "/config.json").toString());
+				if (ver_string_to_int(app.getVersion()) < ver_string_to_int(info.dependencies?.trem ?? "0.0.0")) {log(`Plugin failed to load (${i})`, 2, "plugin", "load_plugin");} else {
+					log(`Plugin loaded successfully (${i})`, 1, "plugin", "load_plugin");
+					if (f.start) f.start();
+				}
+				plugin_info[i] = {
+					f,
+					version      : info.version ?? "0.0.0",
+					description  : info.description ?? "作者未添加說明",
+					author       : info.author ?? ["TREM"],
+					dependencies : info.dependencies ?? {},
+					link         : info.link ?? "https://github.com/ExpTechTW/TREM-Lite",
+					config,
+				};
+			} else {log(`Plugin undefined (${i})`, 2, "plugin", "load_plugin");}
 		} catch (err) {
 			log(`Unable to load plugin (${i}) >> ${err}`, 3, "plugin", "load_plugin");
 		}
