2ad1e67efd811090e9b1141808a46d74eabbf813
Kamiya <58339640+kamiya10@users.noreply.github.com>
2023-07-02 08:06:18 +0800

    style: lint

--- src/core/plugin.js
+++ src/core/plugin.js
@@ -14,12 +14,14 @@ function load_plugin() {
 				const f = reload(Path + i + "/index.js");
 				const info = JSON.parse(fs.readFileSync(Path + i + "/trem.json").toString());
 				const config = JSON.parse(fs.readFileSync(Path + i + "/config.json").toString());
+
 				if (ver_string_to_int(app.getVersion()) < ver_string_to_int(info.dependencies?.trem ?? "0.0.0")) {
 					log(`Plugin failed to load (${i})`, 2, "plugin", "load_plugin");
 				} else {
 					log(`Plugin loaded successfully (${i})`, 1, "plugin", "load_plugin");
 					if (f.start) f.start();
 				}
+
 				plugin_info[i] = {
 					f,
 					version      : info.version ?? "0.0.0",
@@ -35,5 +37,6 @@ function load_plugin() {
 		} catch (err) {
 			log(`Unable to load plugin (${i}) >> ${err}`, 3, "plugin", "load_plugin");
 		}
+
 	plugin.emit("loaded", plugin_info);
 }
