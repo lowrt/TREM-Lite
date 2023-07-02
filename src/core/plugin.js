18daf7ce704e12e29aa8d8a445ad3f94ea67d563
Kamiya <58339640+kamiya10@users.noreply.github.com>
2023-07-02 08:03:43 +0800

    style: lint

--- src/core/plugin.js
+++ src/core/plugin.js
@@ -14,7 +14,9 @@ function load_plugin() {
 				const f = reload(Path + i + "/index.js");
 				const info = JSON.parse(fs.readFileSync(Path + i + "/trem.json").toString());
 				const config = JSON.parse(fs.readFileSync(Path + i + "/config.json").toString());
-				if (ver_string_to_int(app.getVersion()) < ver_string_to_int(info.dependencies?.trem ?? "0.0.0")) {log(`Plugin failed to load (${i})`, 2, "plugin", "load_plugin");} else {
+				if (ver_string_to_int(app.getVersion()) < ver_string_to_int(info.dependencies?.trem ?? "0.0.0")) {
+					log(`Plugin failed to load (${i})`, 2, "plugin", "load_plugin");
+				} else {
 					log(`Plugin loaded successfully (${i})`, 1, "plugin", "load_plugin");
 					if (f.start) f.start();
 				}
@@ -27,7 +29,9 @@ function load_plugin() {
 					link         : info.link ?? "https://github.com/ExpTechTW/TREM-Lite",
 					config,
 				};
-			} else {log(`Plugin undefined (${i})`, 2, "plugin", "load_plugin");}
+			} else {
+				log(`Plugin file not found (${i})`, 2, "plugin", "load_plugin");
+			}
 		} catch (err) {
 			log(`Unable to load plugin (${i}) >> ${err}`, 3, "plugin", "load_plugin");
 		}
