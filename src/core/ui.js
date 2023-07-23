bc9bbd87e83b03bb2ef15c01110cc21940383140
whes1015 <whes1015@gmail.com>
2023-07-23 22:47:03 +0800

    refactor: hotkey

--- src/core/ui.js
+++ src/core/ui.js
@@ -1,11 +1,7 @@
 /* eslint-disable no-undef */
 document.onkeydown = (event) => {
-	if (event.key == "F11")
-		ipcRenderer.send("toggleFullscreen");
-	else if (event.key == "F12")
-		ipcRenderer.send("openDevtool");
-	else if (event.ctrlKey && event.shiftKey && event.key.toLocaleLowerCase() == "i")
-		ipcRenderer.send("openDevtool");
-	else if (event.ctrlKey && event.key.toLocaleLowerCase() == "r")
-		ipcRenderer.send("reload");
+	if (event.key == "F11") ipcRenderer.send("toggleFullscreen");
+	else if (event.key == "F12") ipcRenderer.send("openDevtool");
+	else if (event.ctrlKey && event.shiftKey && event.key.toLocaleLowerCase() == "i") ipcRenderer.send("openDevtool");
+	else if (event.ctrlKey && event.key.toLocaleLowerCase() == "r") ipcRenderer.send("reload");
 };
