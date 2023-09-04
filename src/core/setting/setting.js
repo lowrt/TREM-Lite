91b5b2f492611dc2f7a98b71433a4937fb417673
whes1015 <whes1015@gmail.com>
2023-09-04 13:25:35 +0800

    fix: button

--- src/core/setting/setting.js
+++ src/core/setting/setting.js
@@ -307,9 +307,9 @@ for (const i of plugin_list)
 		const button = document.createElement("button");
 		button.type = "button";
 		button.onclick = () => {
-			shell.showItemInFolder(Path + i + "/config.json");
+			shell.showItemInFolder(`${Path}\\${i}\\config.json`);
 		};
-		button.innerHTML = "<span>Config 配置文件</span><svg class=\"icon\" width=\"16\" height=\"16\" fill=\"none\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M6.25 4.75a1.5 1.5 0 0 0-1.5 1.5v11.5a1.5 1.5 0 0 0 1.5 1.5h11.5a1.5 1.5 0 0 0 1.5-1.5v-4a1 1 0 1 1 2 0v4a3.5 3.5 0 0 1-3.5 3.5H6.25a3.5 3.5 0 0 1-3.5-3.5V6.25a3.5 3.5 0 0 1 3.5-3.5h4a1 1 0 1 1 0 2h-4Zm6.5-1a1 1 0 0 1 1-1h6.5a1 1 0 0 1 1 1v6.5a1 1 0 1 1-2 0V6.164l-4.793 4.793a1 1 0 1 1-1.414-1.414l4.793-4.793H13.75a1 1 0 0 1-1-1Z\" fill=\"#ffffff\"/></svg>";
+		button.innerHTML = "<span>Config 配置文件</span><svg class=\"icon\" width=\"16\" height=\"16\" fill=\"none\" viewBox=\"0 0 24 24\"><path d=\"M6.25 4.75a1.5 1.5 0 0 0-1.5 1.5v11.5a1.5 1.5 0 0 0 1.5 1.5h11.5a1.5 1.5 0 0 0 1.5-1.5v-4a1 1 0 1 1 2 0v4a3.5 3.5 0 0 1-3.5 3.5H6.25a3.5 3.5 0 0 1-3.5-3.5V6.25a3.5 3.5 0 0 1 3.5-3.5h4a1 1 0 1 1 0 2h-4Zm6.5-1a1 1 0 0 1 1-1h6.5a1 1 0 0 1 1 1v6.5a1 1 0 1 1-2 0V6.164l-4.793 4.793a1 1 0 1 1-1.414-1.414l4.793-4.793H13.75a1 1 0 0 1-1-1Z\" fill=\"#ffffff\"/></svg>";
 		item_content.append(button);
 
 		const link = document.createElement("button");
@@ -317,7 +317,7 @@ for (const i of plugin_list)
 		link.onclick = () => {
 			shell.openExternal(info.link ?? "https://github.com/ExpTechTW/TREM-Lite");
 		};
-		link.innerHTML = "<span>Link</span><svg class=\"icon\" width=\"16\" height=\"16\" fill=\"none\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M6.25 4.75a1.5 1.5 0 0 0-1.5 1.5v11.5a1.5 1.5 0 0 0 1.5 1.5h11.5a1.5 1.5 0 0 0 1.5-1.5v-4a1 1 0 1 1 2 0v4a3.5 3.5 0 0 1-3.5 3.5H6.25a3.5 3.5 0 0 1-3.5-3.5V6.25a3.5 3.5 0 0 1 3.5-3.5h4a1 1 0 1 1 0 2h-4Zm6.5-1a1 1 0 0 1 1-1h6.5a1 1 0 0 1 1 1v6.5a1 1 0 1 1-2 0V6.164l-4.793 4.793a1 1 0 1 1-1.414-1.414l4.793-4.793H13.75a1 1 0 0 1-1-1Z\" fill=\"#ffffff\"/></svg>";
+		link.innerHTML = "<span>Link</span><svg class=\"icon\" width=\"16\" height=\"16\" fill=\"none\" viewBox=\"0 0 24 24\"><path d=\"M6.25 4.75a1.5 1.5 0 0 0-1.5 1.5v11.5a1.5 1.5 0 0 0 1.5 1.5h11.5a1.5 1.5 0 0 0 1.5-1.5v-4a1 1 0 1 1 2 0v4a3.5 3.5 0 0 1-3.5 3.5H6.25a3.5 3.5 0 0 1-3.5-3.5V6.25a3.5 3.5 0 0 1 3.5-3.5h4a1 1 0 1 1 0 2h-4Zm6.5-1a1 1 0 0 1 1-1h6.5a1 1 0 0 1 1 1v6.5a1 1 0 1 1-2 0V6.164l-4.793 4.793a1 1 0 1 1-1.414-1.414l4.793-4.793H13.75a1 1 0 0 1-1-1Z\" fill=\"#ffffff\"/></svg>";
 		item_content.append(link);
 
 		item.appendChild(item_border);
