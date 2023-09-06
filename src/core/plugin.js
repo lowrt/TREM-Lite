fe21be86c9e804622c436d4433450d6ec54121f9
whes1015 <whes1015@gmail.com>
2023-09-06 10:23:51 +0800

    refactor: plugin

--- src/core/plugin.js
+++ src/core/plugin.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 load_plugin();
 function load_plugin() {
+	let error = false;
 	const pluginsFolder = path.join(app.getPath("userData"), "plugins");
 	if (!fs.existsSync(pluginsFolder)) fs.mkdirSync(pluginsFolder);
 
@@ -15,20 +16,22 @@ function load_plugin() {
 		try {
 			if (fs.existsSync(path.join(pluginsFolder, pluginName, "index.js"))) {
 				const f = reload(path.join(pluginsFolder, pluginName, "index.js"));
-				const info = JSON.parse(fs.readFileSync(path.join(pluginsFolder, pluginName, "trem.json"), { encoding: "utf-8" }));
+				let info = {};
+
+				try {
+					info = JSON.parse(fs.readFileSync(path.join(pluginsFolder, pluginName, "trem.json"), { encoding: "utf-8" }));
+				} catch (err) {
+					error = true;
+					log(`Plugin failed to load (${pluginName}) >> Info file failed to load`, 3, "plugin", "load_plugin");
+					continue;
+				}
+
 				let config = {};
 
 				try {
 					config = JSON.parse(fs.readFileSync(path.join(pluginsFolder, pluginName, "config.json"), { encoding: "utf-8" }));
 				} catch (err) {
-					log(`Config failed to load (${pluginName})`, 2, "plugin", "load_plugin");
-				}
-
-				if (ver_string_to_int(app.getVersion()) < ver_string_to_int(info.dependencies?.trem ?? "0.0.0")) {
-					log(`Plugin failed to load (${pluginName})`, 2, "plugin", "load_plugin");
-				} else {
-					if (f && typeof f.start == "function") f.start(plugin, { storage });
-					log(`Plugin loaded successfully (${pluginName})`, 1, "plugin", "load_plugin");
+					log(`Plugin loading warning (${pluginName}) >> Config failed to load`, 2, "plugin", "load_plugin");
 				}
 
 				pluginInfo[pluginName] = {
@@ -39,13 +42,46 @@ function load_plugin() {
 					dependencies : info.dependencies ?? {},
 					link         : info.link ?? "https://github.com/ExpTechTW/TREM-Lite",
 					config,
+					function     : {},
 				};
 			} else {
-				log(`Plugin file not found (${pluginName})`, 2, "plugin", "load_plugin");
+				error = true;
+				log(`Plugin failed to load (${pluginName}) >> Plugin file not found`, 3, "plugin", "load_plugin");
 			}
 		} catch (err) {
-			log(`Unable to load plugin (${pluginName}) >> ${err}`, 3, "plugin", "load_plugin");
+			error = true;
+			log(`Plugin failed to load (${pluginName}) >> Unable to load plugin: ${err}`, 3, "plugin", "load_plugin");
 		}
+
+	for (const pluginName of Object.keys(pluginInfo)) {
+		if (pluginName == "trem") continue;
+		const dependencies = pluginInfo[pluginName].dependencies;
+		if (ver_string_to_int(app.getVersion()) < ver_string_to_int(dependencies?.trem ?? "0.0.0")) {
+			error = true;
+			log(`Plugin failed to load (${pluginName}) >> dependencies (TREM) too old (${app.getVersion()} => ^${dependencies.trem})`, 3, "plugin", "load_plugin");
+		} else {
+			let skip = false;
+			for (let I = 0; I < Object.keys(dependencies).length; I++) {
+				const name = Object.keys(dependencies)[I];
+				if (!pluginInfo[name]) {
+					error = true;
+					skip = true;
+					log(`Plugin failed to load (${pluginName}) >> dependencies (${name}) not found`, 3, "plugin", "load_plugin");
+				} else if (ver_string_to_int(pluginInfo[name].version) < ver_string_to_int(dependencies[name])) {
+					error = true;
+					skip = true;
+					log(`Plugin failed to load (${pluginName}) >> dependencies (${name}) too old (${pluginInfo[name].version} => ^${dependencies[name]})`, 3, "plugin", "load_plugin");
+				}
+			}
+			if (!skip) {
+				const f = pluginInfo[pluginName].f;
+				if (f && typeof f.start == "function") f.start(plugin, { storage, info: pluginInfo[pluginName], pluginInfo });
+				log(`Plugin loaded successfully (${pluginName})`, 1, "plugin", "load_plugin");
+			}
+		}
+	}
+
+	if (!error) document.getElementById("icon-plugin").style.display = "none";
 
 	plugin.emit("loaded", pluginInfo);
 }
