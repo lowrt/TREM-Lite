f6d56b962bf87267b9453ef8aae7afb18b479b54
whes1015 <whes1015@gmail.com>
2023-09-09 13:32:23 +0800

    fix: focus

--- src/core/index/loop.js
+++ src/core/index/loop.js
@@ -554,4 +554,4 @@ setInterval(() => {
 			const set_center = Math.sqrt(pow((center.lat - center_now.lat) * 111) + pow((center.lng - center_now.lng) * 101));
 			TREM.Maps.main.setView((set_center > 10) ? center : center_now, zoom);
 		}
-}, 1000);
+}, 50);
