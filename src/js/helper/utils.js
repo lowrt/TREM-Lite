03a3aef6590726bed12b0ca7efae9f099bb9d297
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-01-13 01:14:01 +0800

    fix(utils): sampleArray index out of bounds

--- src/js/helper/utils.js
+++ src/js/helper/utils.js
@@ -3,6 +3,6 @@
  * @param {Array<T>} arr
  * @returns {T}
  */
-const sampleArray = (arr) => arr[Math.round(Math.random() * arr.length)];
+const sampleArray = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 module.exports = { sampleArray };
