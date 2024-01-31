554d3642ed289e6f8e5e4994264b8da4cfabdd72
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-01-31 22:24:52 +0800

    ref: move function out of module

--- src/js/websocket.js
+++ src/js/websocket.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 const API = require("../js/class/api");
-const { reportListItem } = require("../js/helper/factory");
 
 const api = new API();
 
@@ -22,15 +21,95 @@ api.on(API.Events.Eew, (eew) => {
 
 
 const updateReports = async () => {
+  const { reportListItem, reportIntensityItem, reportIntensityItemGroup } = require("../js/helper/factory");
+  const { extractLocationFromString, toFormattedTimeString } = require("../js/helper/utils");
+  const ElementBuilder = require("../js/class/elementbuilder");
+
   const reports = await api.getReports(20);
 
-  const list = document.getElementById("list-box");
+  const reportList = document.getElementById("list-box");
 
   const frag = new DocumentFragment();
   for (const report of reports)
-    frag.appendChild(reportListItem(report).toElement());
+    frag.appendChild(
+      reportListItem(report)
+        .on("click", (() => {
+          /**
+           * @type {import("../class/api").Report}
+           */
+          // TODO: Move cache to other place
+          let r;
 
-  list.replaceChildren(frag);
+          const updateReportBox = () => {
+            document.getElementById("report-box").setAttribute("data-report-id", this.id);
+            document.getElementById("report-max-intensity").className = `report-max-intensity intensity-${report.int}`;
+            document.getElementById("report-subtitle").textContent = report.no % 1000 ? `編號 ${report.no}` : "小區域有感地震";
+            document.getElementById("report-title").textContent = extractLocationFromString(report.loc);
+            document.getElementById("report-time").textContent = toFormattedTimeString(report.time);
+            document.getElementById("report-location").textContent = report.loc.substring(0, report.loc.indexOf("(")).trim();
+            document.getElementById("report-longitude").textContent = report.lon;
+            document.getElementById("report-latitude").textContent = report.lat;
+            document.getElementById("report-magnitude").textContent = report.mag;
+            document.getElementById("report-depth").textContent = report.depth;
+            document.getElementById("report-intensity-grouped").replaceChildren();
+            document.getElementById("report-intensity-all").replaceChildren();
+          };
+
+          const updateReportIntensityList = () => {
+            const intList = [];
+            for (const area in r.list) {
+              const town = [];
+              for (const station in r.list[area].town)
+                town.push({ ...r.list[area].town[station], name: station });
+              town.sort((a, b) => b.int - a.int);
+              intList.push({ ...r.list[area], name: area, town });
+            }
+
+            // -- grouped intensity
+
+            intList.sort((a, b) => b.int - a.int);
+            const grouped = new DocumentFragment();
+
+            for (const area of intList)
+              grouped.appendChild(reportIntensityItemGroup(area).toElement());
+
+            document.getElementById("report-intensity-grouped").replaceChildren(grouped);
+
+            // -- all intensity
+
+            const intListAll = intList.reduce((acc, area) => (acc.push(...area.town.map(t => (t.station = t.name, t.name = area.name, t))), acc), []).sort((a, b) => b.int - a.int);
+            const all = new DocumentFragment();
+
+            for (const station of intListAll)
+              all.appendChild(reportIntensityItem(station)
+                .addChildren(new ElementBuilder("span")
+                  .setClass(["report-intensity-item-station"])
+                  .setContent(station.station))
+                .toElement());
+
+            document.getElementById("report-intensity-all").replaceChildren(all);
+          };
+
+          return async function() {
+            try {
+              document.getElementById("report-box").classList.add("show");
+
+              updateReportBox();
+
+              if (!r) r = await api.getReport(this.id);
+
+              updateReportIntensityList();
+
+            } catch (error) {
+              console.error(error);
+              document.getElementById("report-box").classList.remove("show");
+            }
+          };
+        })())
+        .toElement());
+
+
+  reportList.replaceChildren(frag);
 };
 
 updateReports();
